488155622fcf0d61d122d9ab6baeff520761caa2
42117-tech-takafumi <takafumitech@gmail.com>
2025-02-26 23:04:17 +0900

    rubocopを修正、実行、cssファイル追加2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  validates :birth_date, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :first_name
@@ -18,7 +19,6 @@ class User < ApplicationRecord
     validates :last_name_kana
   end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+  validates :birth_date, presence: true
 
 end
