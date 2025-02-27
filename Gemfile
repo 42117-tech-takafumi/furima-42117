55f2e300810808bb3de4e912a3a6299bfa61f47f
42117-tech-takafumi <takafumitech@gmail.com>
2025-02-27 20:13:19 +0900

    ユーザー管理機能_FAKER追加、passwordのテスト再実施

--- Gemfile
+++ Gemfile
@@ -47,6 +47,10 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+  gem 'rubocop', '1.71.2', require: false
+  gem 'rspec-rails', '~> 4.0.0'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
@@ -58,15 +62,12 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
-  gem 'rubocop', '1.71.2', require: false
 end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
-  gem 'rspec-rails', '~> 4.0.0'
-  gem 'factory_bot_rails'
 end
 
 group :production do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,9 +45,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
-      it 'passwordが半角英数字混合でない場合は登録できない' do
+      it 'passwordが半角数字のみの場合は登録できない' do
         @user.password="1234567"
         @user.password_confirmation="1234567"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
+      it 'passwordが半角英字のみの場合は登録できない' do
+        @user.password="abcdefg"
+        @user.password_confirmation="abcdefg"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
+      it 'passwordが全角の場合は登録できない' do
+        @user.password="ＡＢＣＤＥＦＧ１２"
+        @user.password_confirmation="ＡＢＣＤＥＦＧ１２"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
       end
